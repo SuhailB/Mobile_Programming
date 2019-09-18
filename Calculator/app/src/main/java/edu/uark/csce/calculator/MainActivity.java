be09949e1084d8bd7e242bc3e5e81ffb193c3009
Suhail Ba Salama <sebasala@uark.edu>
2019-09-18 13:35:16 -0500

    Cleaned and Tested

--- Calculator/app/src/main/java/edu/uark/csce/calculator/MainActivity.java
+++ Calculator/app/src/main/java/edu/uark/csce/calculator/MainActivity.java
@@ -15,9 +15,9 @@ import java.text.DecimalFormat;
 public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private TextView CalDisplay;
-    private static final String DIGITS = "0123456789.";
     DecimalFormat df1 = new DecimalFormat("@###########");
     DecimalFormat df2 = new DecimalFormat("0.######E0");
+
     private Boolean userHasPressedDigit = false;
 
     private double newOperand;
@@ -26,15 +26,12 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     // ongoingOperator types
     private static final String ADD = "+";
-    private static final String SUBTRACT = "-";
-    private static final String MULTIPLY = "*";
-    private static final String DIVIDE = "/";
-
-
+    private static final String SUB = "-";
+    private static final String MUL = "*";
+    private static final String DIV = "/";
     private static final String BACK = "B";
     private static final String CLEAR = "C";
-
-    private static final String TOGGLESIGN = "+/-";
+    private static final String SIGN = "+/-";
 
     
     @Override
@@ -42,34 +39,31 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        CalDisplay = (TextView) findViewById(R.id.textViewDisplay);
+
         newOperand = 0;
         ongoingOperand = 0;
         ongoingOperator = "";
 
-        final ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
-
-        CalDisplay = (TextView) findViewById(R.id.textViewDisplay);
-
-        findViewById(R.id.button0).setOnClickListener(this);
-        findViewById(R.id.button1).setOnClickListener(this);
-        findViewById(R.id.button2).setOnClickListener(this);
-        findViewById(R.id.button3).setOnClickListener(this);
-        findViewById(R.id.button4).setOnClickListener(this);
-        findViewById(R.id.button5).setOnClickListener(this);
-        findViewById(R.id.button6).setOnClickListener(this);
-        findViewById(R.id.button7).setOnClickListener(this);
-        findViewById(R.id.button8).setOnClickListener(this);
-        findViewById(R.id.button9).setOnClickListener(this);
-        findViewById(R.id.buttonAdd).setOnClickListener(this);
-        findViewById(R.id.buttonSubtract).setOnClickListener(this);
-        findViewById(R.id.buttonMultiply).setOnClickListener(this);
-        findViewById(R.id.buttonDivide).setOnClickListener(this);
-        findViewById(R.id.buttonToggleSign).setOnClickListener(this);
-        findViewById(R.id.buttonDecimalPoint).setOnClickListener(this);
-        findViewById(R.id.buttonEquals).setOnClickListener(this);
-        findViewById(R.id.buttonBack).setOnClickListener(this);
-        findViewById(R.id.buttonClear).setOnClickListener(this);
+        findViewById(R.id.b0).setOnClickListener(this);
+        findViewById(R.id.b1).setOnClickListener(this);
+        findViewById(R.id.b2).setOnClickListener(this);
+        findViewById(R.id.b3).setOnClickListener(this);
+        findViewById(R.id.b4).setOnClickListener(this);
+        findViewById(R.id.b5).setOnClickListener(this);
+        findViewById(R.id.b6).setOnClickListener(this);
+        findViewById(R.id.b7).setOnClickListener(this);
+        findViewById(R.id.b8).setOnClickListener(this);
+        findViewById(R.id.b9).setOnClickListener(this);
+        findViewById(R.id.bAdd).setOnClickListener(this);
+        findViewById(R.id.bSub).setOnClickListener(this);
+        findViewById(R.id.bMul).setOnClickListener(this);
+        findViewById(R.id.bDiv).setOnClickListener(this);
+        findViewById(R.id.bSign).setOnClickListener(this);
+        findViewById(R.id.bPoint).setOnClickListener(this);
+        findViewById(R.id.bEquals).setOnClickListener(this);
+        findViewById(R.id.bBack).setOnClickListener(this);
+        findViewById(R.id.bClear).setOnClickListener(this);
      
 
 
@@ -78,118 +72,98 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     public void onClick(View v) {
 
         String buttonPressed = ((Button) v).getText().toString();
-//        Log.e("Button Pressed",buttonPressed);
 
         //if button pressed is a digit or .
-        if (DIGITS.contains(buttonPressed)) 
-        {
-            //case when didg
-            if (userHasPressedDigit) 
-            {
-                //if no decimal point is inserted again append the 
+        if ("0123456789.".contains(buttonPressed)) {
+            //one digit has been pressed
+            if (userHasPressedDigit) {
+                //if no decimal point is inserted again append the digit
                 if (!(buttonPressed.equals(".") && CalDisplay.getText().toString().contains("."))) {
-                    // ERROR PREVENTION
-                    // Eliminate entering multiple decimals
                     CalDisplay.append(buttonPressed);
                 }
             } 
-            else 
-            {
-                // ERROR PREVENTION
+            else {
                 // This will avoid error if only the decimal is hit before an ongoingOperator, by placing a leading zero before the decimal
-                if (buttonPressed.equals("."))
+                if (buttonPressed.equals(".")) {
                     CalDisplay.setText(0 + buttonPressed);
-                else
+                    userHasPressedDigit = true;
+                }
+                //putting zero as the first digit is not allowed
+                else if(buttonPressed.equals("0"))
+                    userHasPressedDigit = false;
+                //any other digit is okay
+                else {
                     CalDisplay.setText(buttonPressed);
-                userHasPressedDigit = true;
+                    userHasPressedDigit = true;
+                }
             }
 
         }
-        //if B is pressed
+
+        //if Back button is pressed is pressed
         else if(buttonPressed.equals("B")){
+            //if there are still digits in the screen, delete them
             if(CalDisplay.getText().length()>1)
                 CalDisplay.setText(CalDisplay.getText().subSequence(0,CalDisplay.getText().length()-1));
             else {
-                performOperation(buttonPressed);
-                
-                if(Math.abs(getResult())>999999999 || Math.abs(getResult())<0.000001 && getResult()!=0)
-                    CalDisplay.setText(df2.format(getResult()));
+                doOperation(buttonPressed);
+                if(Math.abs(newOperand)>999999999 || Math.abs(newOperand)<0.000001 && newOperand!=0)
+                    CalDisplay.setText(df2.format(newOperand));
                 else
-                    CalDisplay.setText(df1.format(getResult()));
+                    CalDisplay.setText(df1.format(newOperand));
                 userHasPressedDigit = false;
-//                Log.e("B Pressed",buttonPressed);
             }
         }
         //if any other button is pressed
         else {
             // operation was pressed
             if (userHasPressedDigit) {
-                setOperand(Double.parseDouble(CalDisplay.getText().toString()));
+                newOperand = Double.parseDouble(CalDisplay.getText().toString());
                 userHasPressedDigit = false;
             }
-            performOperation(buttonPressed);
-            if(Math.abs(getResult())>999999999 || Math.abs(getResult())<0.00000001 && getResult()!=0)
-                CalDisplay.setText(df2.format(getResult()));
+            doOperation(buttonPressed);
+            if(Math.abs(newOperand)>999999999 || Math.abs(newOperand)<0.000001 && newOperand!=0)
+                CalDisplay.setText(df2.format(newOperand));
             else
-                CalDisplay.setText(df1.format(getResult()));
+                CalDisplay.setText(df1.format(newOperand));
         }
     }
 
-    public void setOperand(double operand) {
-        newOperand = operand;
-    }
-
-    public double getResult() {
-        return newOperand;
-    }
-
-
-    public String toString() {
-        return Double.toString(newOperand);
-    }
-
-    protected double performOperation(String operator) {
+    protected void doOperation(String operator) {
 
         switch (operator) {
             case CLEAR:
-                newOperand = 0;
-                ongoingOperator = "";
-                ongoingOperand = 0;
-                break;
             case BACK:
                 newOperand = 0;
                 ongoingOperator = "";
                 ongoingOperand = 0;
                 break;
-            case TOGGLESIGN:
+            case SIGN:
                 newOperand = -newOperand;
                 break;
             default:
-                performOngoingOperation();
+                evaluateOngoingOperation();
                 ongoingOperator = operator;
                 ongoingOperand = newOperand;
                 break;
         }
-
-        return newOperand;
     }
 
-    private void performOngoingOperation() {
+    protected void evaluateOngoingOperation() {
 
         switch (ongoingOperator) {
             case ADD:
                 newOperand = ongoingOperand + newOperand;
                 break;
-            case SUBTRACT:
+            case SUB:
                 newOperand = ongoingOperand - newOperand;
                 break;
-            case MULTIPLY:
+            case MUL:
                 newOperand = ongoingOperand * newOperand;
                 break;
-            case DIVIDE:
-                if (newOperand != 0) {
+            case DIV:
+                if (newOperand != 0)
                     newOperand = ongoingOperand / newOperand;
-                }
                 break;
         }
 
