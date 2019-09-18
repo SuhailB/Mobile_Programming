22dddfd09b2014e74ac1904d02c80bfd46b93688
Suhail Ba Salama <sebasala@uark.edu>
2019-09-18 01:24:32 -0500

    Classes merged

--- Calculator/app/src/main/java/edu/uark/csce/calculator/MainActivity.java
+++ Calculator/app/src/main/java/edu/uark/csce/calculator/MainActivity.java
@@ -16,19 +16,37 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     private TextView CalDisplay;
     private static final String DIGITS = "0123456789.";
-    DecimalFormat df = new DecimalFormat("@###########");
+    DecimalFormat df = new DecimalFormat("0.######E0");
     private Boolean userHasPressedDigit = false;
 
-    CalculatorLogic CalLogic;
+    private double newOperand;
+    private double ongoingOperand;
+    private String ongoingOperator;
+
+    // ongoingOperator types
+    private static final String ADD = "+";
+    private static final String SUBTRACT = "-";
+    private static final String MULTIPLY = "*";
+    private static final String DIVIDE = "/";
+
+
+    private static final String BACK = "B";
+    private static final String CLEAR = "C";
+
+    private static final String TOGGLESIGN = "+/-";
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        newOperand = 0;
+        ongoingOperand = 0;
+        ongoingOperator = "";
+
         final ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
-
-        CalLogic = new CalculatorLogic();
 
         CalDisplay = (TextView) findViewById(R.id.textViewDisplay);
 
@@ -42,7 +60,6 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         findViewById(R.id.button7).setOnClickListener(this);
         findViewById(R.id.button8).setOnClickListener(this);
         findViewById(R.id.button9).setOnClickListener(this);
-
         findViewById(R.id.buttonAdd).setOnClickListener(this);
         findViewById(R.id.buttonSubtract).setOnClickListener(this);
         findViewById(R.id.buttonMultiply).setOnClickListener(this);
@@ -60,63 +77,112 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
     public void onClick(View v) {
 
         String buttonPressed = ((Button) v).getText().toString();
-        Log.e("Button Pressed",buttonPressed);
-        // String digits = "0123456789.";
+//        Log.e("Button Pressed",buttonPressed);
 
         //if button pressed is a digit or .
-        if (DIGITS.contains(buttonPressed)) {
-
-            // digit was pressed
-            if (userHasPressedDigit) {
-
-//				Toast.makeText(getApplicationContext(), "1", Toast.LENGTH_SHORT).show();
-
-                if (buttonPressed.equals(".") && CalDisplay.getText().toString().contains(".")) {
+        if (DIGITS.contains(buttonPressed)) 
+        {
+            //case when didg
+            if (userHasPressedDigit) 
+            {
+                //if no decimal point is inserted again append the 
+                if (!(buttonPressed.equals(".") && CalDisplay.getText().toString().contains("."))) {
                     // ERROR PREVENTION
                     // Eliminate entering multiple decimals
-//					Toast.makeText(getApplicationContext(), "2", Toast.LENGTH_SHORT).show();
-                } else {
-
                     CalDisplay.append(buttonPressed);
-//					Toast.makeText(getApplicationContext(), "3", Toast.LENGTH_SHORT).show();
                 }
-
-            } else {
-
-                if (buttonPressed.equals(".")) {
-                    // ERROR PREVENTION
-                    // This will avoid error if only the decimal is hit before an operator, by placing a leading zero before the decimal
+            } 
+            else 
+            {
+                // ERROR PREVENTION
+                // This will avoid error if only the decimal is hit before an ongoingOperator, by placing a leading zero before the decimal
+                if (buttonPressed.equals("."))
                     CalDisplay.setText(0 + buttonPressed);
-                } else {
+                else
                     CalDisplay.setText(buttonPressed);
-                }
-
                 userHasPressedDigit = true;
             }
 
         }
+        //if B is pressed
         else if(buttonPressed.equals("B")){
             if(CalDisplay.getText().length()>1)
                 CalDisplay.setText(CalDisplay.getText().subSequence(0,CalDisplay.getText().length()-1));
             else {
-                CalLogic.performOperation(buttonPressed);
-                CalDisplay.setText(df.format(CalLogic.getResult()));
+                performOperation(buttonPressed);
+                CalDisplay.setText(df.format(getResult()));
                 userHasPressedDigit = false;
-                Log.e("B Pressed",buttonPressed);
+//                Log.e("B Pressed",buttonPressed);
             }
         }
         //if any other button is pressed
         else {
             // operation was pressed
             if (userHasPressedDigit) {
-
-                CalLogic.setOperand(Double.parseDouble(CalDisplay.getText().toString()));
+                setOperand(Double.parseDouble(CalDisplay.getText().toString()));
                 userHasPressedDigit = false;
             }
+            performOperation(buttonPressed);
+            CalDisplay.setText(df.format(getResult()));
+        }
+    }
 
-            CalLogic.performOperation(buttonPressed);
-            CalDisplay.setText(df.format(CalLogic.getResult()));
+    public void setOperand(double operand) {
+        newOperand = operand;
+    }
 
+    public double getResult() {
+        return newOperand;
+    }
+
+
+    public String toString() {
+        return Double.toString(newOperand);
+    }
+
+    protected double performOperation(String operator) {
+
+        switch (operator) {
+            case CLEAR:
+                newOperand = 0;
+                ongoingOperator = "";
+                ongoingOperand = 0;
+                break;
+            case BACK:
+                newOperand = 0;
+                ongoingOperator = "";
+                ongoingOperand = 0;
+                break;
+            case TOGGLESIGN:
+                newOperand = -newOperand;
+                break;
+            default:
+                performOngoingOperation();
+                ongoingOperator = operator;
+                ongoingOperand = newOperand;
+                break;
+        }
+
+        return newOperand;
+    }
+
+    private void performOngoingOperation() {
+
+        switch (ongoingOperator) {
+            case ADD:
+                newOperand = ongoingOperand + newOperand;
+                break;
+            case SUBTRACT:
+                newOperand = ongoingOperand - newOperand;
+                break;
+            case MULTIPLY:
+                newOperand = ongoingOperand * newOperand;
+                break;
+            case DIVIDE:
+                if (newOperand != 0) {
+                    newOperand = ongoingOperand / newOperand;
+                }
+                break;
         }
 
     }
